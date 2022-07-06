a645dbe61bad05721493a4aa0dfa215d3ee5e3bc
zs-zc <3011121187@qq.com>
2022-07-06 11:25:20 +0800

    fix:表单表格

---
+++ src/views/myDialog/myDialog2/diaDetail.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <el-dialog
+      :title="DialogTitle"
+      :visible.sync="dialogVisible"
+      width="70%"
+      :before-close="handleClose"
+    >
+      <slot></slot>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    DialogTitle: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+    openDia(row) {
+      this.dialogVisible = true;
+      console.log(row, "row");
+    },
+    closeDia() {
+      this.dialogVisible = false;
+    },
+  },
+};
+</script>
--- src/views/myDialog/myDialog2/index.vue
+++ src/views/myDialog/myDialog2/index.vue
@@ -1,10 +1,104 @@
 <template>
-  <div>弹窗2</div>
+  <div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column fixed prop="date" label="日期" width="150">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="province" label="省份" width="120">
+      </el-table-column>
+      <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址" width="300">
+      </el-table-column>
+      <el-table-column prop="zip" label="邮编" width="120"> </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--  -->
+    <dialog-detail ref="diaDetail" :DialogTitle="'详情'">
+      <el-form ref="form" :model="form" label-width="80px" :form="form">
+        <el-form-item label="日期">
+          <el-input v-model="form.date"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="省份">
+          <el-input v-model="form.province"></el-input>
+        </el-form-item>
+        <el-form-item label="市区">
+          <el-input v-model="form.city"></el-input>
+        </el-form-item>
+        <el-form-item label="邮编">
+          <el-input v-model="form.zip"></el-input>
+        </el-form-item>
+      </el-form>
+    </dialog-detail>
+
+    <div>
+      <el-button>返回</el-button>
+      <el-button>提交</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import dialogDetail from "./diaDetail.vue";
+export default {
+  components: { dialogDetail },
+  data() {
+    return {
+      form: {},
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1519 弄",
+          zip: 200333,
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1516 弄",
+          zip: 200333,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(row) {
+      this.$refs.diaDetail.openDia(row);
+      this.form = { ...row };
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+
+
+
